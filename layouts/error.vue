1630d7ae54100b0e139a63a7c3f1685f6b567aa5
Mauricio <mauricio.pereira@mobly.com.br>
2022-10-22 11:28:54 -0300

    feat: add error page

---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="flex items-center justify-center w-full pt-40"
+  >
+    <div>
+      <div
+        class="flex justify-center"
+      >
+        <p
+          class="text-3xl opacity-75 text-bertolt-primary"
+        >
+          Página não encontrada :/
+        </p>
+      </div>
+      <img
+        :src="require('../assets/no-data-cuate.png')"
+        class="h-96"
+        alt=""
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'error' // you can set a custom layout for the error page
+  }
+</script>
