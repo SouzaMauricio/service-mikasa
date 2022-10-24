88bd24e48b6444493638f5dcb8d3d00cec386b03
Mauricio <mauricio.pereira@mobly.com.br>
2022-10-24 18:03:28 -0300

    fix: adjustments api

--- gateway/armin/ArminAxiosClient.js
+++ gateway/armin/ArminAxiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 // import { store } from '@/store/store'
 
 const arminAxios = axios.create({
-  baseURL: 'https://api.corretoraandreia.com.br',
+  baseURL: 'https://andreia.totustech.com.br',
   timeout: 30000
 })
 
