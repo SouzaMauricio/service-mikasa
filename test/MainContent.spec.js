c7877be47f00ac2e4492119d15521c34c4bd18a5
Mauricio <mauricio.pereira@mobly.com.br>
2022-10-24 16:34:55 -0300

    test: test

--- test/MainContent.spec.js
+++ test/MainContent.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import NuxtLogo from '@/components/NuxtLogo.vue'
+import NuMainContentxtLogo from '@/components/main/MainContent.vue'
 
 describe('NuxtLogo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(NuxtLogo)
+    const wrapper = mount(MainContent)
     expect(wrapper.vm).toBeTruthy()
   })
 })
